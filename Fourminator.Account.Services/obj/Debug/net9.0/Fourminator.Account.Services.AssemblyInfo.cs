4dda49c88c8f0d6eaba0b540df958d0836e496bf
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-18 21:39:30 +0100

    Added Data Class Library

--- Fourminator.Account.Services/obj/Debug/net9.0/Fourminator.Account.Services.AssemblyInfo.cs
+++ Fourminator.Account.Services/obj/Debug/net9.0/Fourminator.Account.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fourminator.Account.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22779701a0529403fcef0a8db570a2a0879b733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ecb93fce79a7914c65c5592de8ff60c03d423e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fourminator.Account.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fourminator.Account.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
