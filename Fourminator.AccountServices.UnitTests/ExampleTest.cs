e1af02251bdbc58a60dd00762146bc8c33fc48b1
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-19 21:50:01 +0100

    Added Test Frameworks

---
+++ Fourminator.AccountServices.UnitTests/ExampleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fourminator.AccountServices.UnitTests
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void ExampleTest_ShouldAddTwoNumbers()
+        {
+            // Arrange
+            int a = 1;
+            int b = 2;
+            int expected = 3;
+            // Act
+            int actual = a + b;
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
