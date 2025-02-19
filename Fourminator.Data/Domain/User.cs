e0e478f219edc63d654e2a9adcc046d0dc24ee88
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-19 21:39:23 +0100

    Added User Model

---
+++ Fourminator.Data/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fourminator.Data.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; } = new Guid();
+        public string ExternalId { get; set; }
+        public string Nickname { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
