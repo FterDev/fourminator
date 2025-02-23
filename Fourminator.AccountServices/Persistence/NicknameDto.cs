33f692be408ebff44ecead26092466dc22476da0
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-23 13:24:28 +0100

    Added NicknameDto

---
+++ Fourminator.AccountServices/Persistence/NicknameDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fourminator.AccountServices.Persistence
+{
+    public class NicknameDto
+    {
+        public string Nickname { get; set; }
+    }
+}
