40555f1b2c5567f7eec32ccfdaa961c2c2d247bb
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-04-17 19:59:42 +0200

    Configured translate

--- fourminator.client/src/app/app.component.ts
+++ fourminator.client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+import translateEn from '../../public/i18n/en.json';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor(private translateService: TranslateService)
+  {
+    translateService.setTranslation('en', translateEn);
+    translateService.setDefaultLang('en');
+  }
   title = 'fourminator.client';
 }
--- fourminator.client/src/app/app.config.ts
+++ fourminator.client/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {provideTranslateService} from "@ngx-translate/core";
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideTranslateService({
+      defaultLanguage: 'en'
+    }),
+    provideRouter(routes)
+  ]
 };
