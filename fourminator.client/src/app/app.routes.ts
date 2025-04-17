d501eca34e236d92706e709f22f9820d69cee2c5
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-04-17 19:40:18 +0200

    Added root router

--- fourminator.client/src/app/app.routes.ts
+++ fourminator.client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: "full", component: LandingPageComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
