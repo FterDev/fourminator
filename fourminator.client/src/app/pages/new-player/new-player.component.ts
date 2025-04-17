4be351a2a69f3bc5673d80ddce554e6ad5b2d67c
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-04-17 19:33:58 +0200

    Added base pages

---
+++ fourminator.client/src/app/pages/new-player/new-player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fm-new-player',
+  imports: [],
+  templateUrl: './new-player.component.html',
+  styleUrl: './new-player.component.scss'
+})
+export class NewPlayerComponent {
+
+}
