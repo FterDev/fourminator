c8a5674ae2b3cfcc3f7205d42526b585ae2fb1ba
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-04-18 14:14:13 +0200

    Implements new player form
    Replaces placeholder text in the new player page with a functional form.
    The form includes fields for nickname, terms of use acceptance, and a submit button.
    Also includes basic styling for the form.
    Adds i18n entries for form elements.

--- fourminator.client/src/app/pages/new-player/new-player.component.ts
+++ fourminator.client/src/app/pages/new-player/new-player.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NewPlayerFormComponent } from "../../shared/components/new-player-form/new-player-form.component";
 
 @Component({
   selector: 'fm-new-player',
-  imports: [],
+  imports: [NewPlayerFormComponent],
   templateUrl: './new-player.component.html',
   styleUrl: './new-player.component.scss'
 })
--- fourminator.client/src/app/shared/components/new-player-form/new-player-form.component.ts
+++ fourminator.client/src/app/shared/components/new-player-form/new-player-form.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'fm-new-player-form',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule],
   templateUrl: './new-player-form.component.html',
   styleUrl: './new-player-form.component.scss'
 })
