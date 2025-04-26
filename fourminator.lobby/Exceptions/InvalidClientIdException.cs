a687ef65ee63888c81c34d3dbc8eba4beaefacb3
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-04-26 17:55:22 +0200

    Adds user connection/disconnection methods
    Implements methods for connecting and disconnecting users.
    
    DisconnectUser now throws an exception when attempting to
    disconnect a user with an invalid client ID. This improves error
    handling when a non-existent client ID is provided.

---
+++ fourminator.lobby/Exceptions/InvalidClientIdException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fourminator.Lobby.Exceptions
+{
+    internal class InvalidClientIdException : Exception
+    {
+        public InvalidClientIdException(Exception innerException) : base(message: "An invalid or non-existend client id was provided!", innerException: innerException) {
+        
+        }
+    }
+}
--- fourminator.lobby/Persistence/User.cs
+++ fourminator.lobby/Persistence/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using fourminator.Lobby.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,26 @@ namespace fourminator.Lobby.Persistence
         public required string ClientId { get; set; }
         public required string Nickname { get; set; }
         public ushort UniqueIdentifier { get; set; }
+
+        internal Task ConnectUser(IList<User> connectedUsers)
+        {
+            connectedUsers.Add(this);
+            return Task.CompletedTask;
+        }
+
+        internal Task DisconnectUser(IList<User> connectedUsers, string clientId)
+        {
+            try
+            {
+                var user = connectedUsers.Where(cu => cu.ClientId == clientId).First();
+                connectedUsers.Remove(user);
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidClientIdException(ex);
+            }
+            
+        }
     }
 }
